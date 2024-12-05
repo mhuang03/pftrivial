a4d423252be5933649322427f1062e5bdc981aee
mhuang03 <huangmichael150@gmail.com>
2024-12-04 19:34:51 -0600

    add ./ everywhere

--- build.js
+++ build.js
@@ -10,25 +10,25 @@ if (process.argv.length > 3) {
 const DEV = process.argv.length == 3 && process.argv[2] === "dev";
 
 // create output folders if nonexistent
-fs.mkdirSync("public", { recursive: true });
-fs.mkdirSync("netlify/functions", { recursive: true });
+fs.mkdirSync("./public", { recursive: true });
+fs.mkdirSync("./netlify/functions", { recursive: true });
 
 // compile html
-let html = pug.compileFile("pug/index.pug")({ DEV });
-fs.writeFileSync("public/index.html", html, "utf8");
-fs.copyFileSync("public/index.html", "public/404.html");
+let html = pug.compileFile("./pug/index.pug")({ DEV });
+fs.writeFileSync("./public/index.html", html, "utf8");
+fs.copyFileSync("./public/index.html", "./public/404.html");
 
 // compile js
 await esbuild.build({
   entryPoints: [
-    { in: "lib/index.js", out: "public/index" },
-    { in: "lib/api.js", out: "netlify/functions/api" },
+    { in: "./lib/index.js", out: "./public/index" },
+    { in: "./lib/api.js", out: "./netlify/functions/api" },
   ],
-  outdir: "/",
+  outdir: "./",
   bundle: true,
   write: true,
   minify: true,
 });
 
 // copy over static files
-fs.cpSync("static/", "public/", { recursive: true, overwrite: true });
+fs.cpSync("./static/", "./public/", { recursive: true, overwrite: true });
