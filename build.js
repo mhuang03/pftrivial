ab45f7af9296b08d54bf2f32fc66cc46dd79b139
mhuang03 <huangmichael150@gmail.com>
2024-12-04 19:20:54 -0600

    add random number functionality; change build chain to use esbuild

--- build.js
+++ build.js
@@ -1,41 +1,34 @@
 import fs from "node:fs";
 import pug from "pug";
+import esbuild from "esbuild";
 
+// handle DEV behavior
 if (process.argv.length > 3) {
   console.error("Expected at most one argument!");
   process.exit(1);
 }
 const DEV = process.argv.length == 3 && process.argv[2] === "dev";
 
-let data = {};
+// create output folders if nonexistent
+fs.mkdirSync("public", { recursive: true });
+fs.mkdirSync("netlify/functions", { recursive: true });
 
-const readWords = (fName) => {
-  return new Promise((resolve, reject) => {
-    fs.readFile(`words/${fName}.txt`, "utf8", (err, fileContents) => {
-      if (err) reject(err);
-      else {
-        data[fName] = fileContents.trim().split(/\r?\n/);
-        resolve();
-      }
-    });
-  });
-};
-
-await ["adjectives", "containments", "degraders", "methods", "nounsOther", "nounsSetlike", "theorems", "uses"].reduce(
-  (prev, curr) => {
-    return prev.then(() => readWords(curr)).catch((e) => console.log(e));
-  },
-  Promise.resolve()
-);
-
-let data_str = JSON.stringify(data, null, 0);
-let html = pug.compileFile("pug/index.pug")({ data_str, DEV });
-
+// compile html
+let html = pug.compileFile("pug/index.pug")({ DEV });
 fs.writeFileSync("public/index.html", html, "utf8");
 fs.copyFileSync("public/index.html", "public/404.html");
 
-if (!DEV) {
-  let apijs = fs.readFileSync("netlify/functions/api.js", "utf8");
-  apijs = apijs.replace("const data = null;", `const data = ${data_str};`);
-  fs.writeFileSync("netlify/functions/api.js", apijs, "utf8");
-}
+// compile js
+await esbuild.build({
+  entryPoints: [
+    { in: "lib/index.js", out: "public/index" },
+    { in: "lib/api.js", out: "netlify/functions/api" },
+  ],
+  outdir: "/",
+  bundle: true,
+  write: true,
+  minify: true,
+});
+
+// copy over static files
+fs.cpSync("static/", "public/", { recursive: true, overwrite: true });
